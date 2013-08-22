c4bc12491c45277689b02e2946d886c47e9318f7
LNSEAB <lnseab@gmail.com>
2013-08-22 09:15:34 +0900

    最初のコミット

---
+++ src/resource.h
@@ -0,0 +1,12 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDR_MAINMENU                            102
+#define IDD_MAINWINDOW                          103
+#define IDC_RESULT                              1003
+#define IDM_____O_1                             40000
+#define IDM_____S_1                             40001
+#define IDM_____Q_1                             40002
+#define IDC_EXTFILTER                           40003
+#define IDC_BUTTON1                             40004
